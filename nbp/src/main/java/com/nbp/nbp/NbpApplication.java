8d9b4277746a55a7d56d2cc80352581283a3f451
Pavo <pzornija1@etf.unsa.ba>
2022-05-22 17:22:28 +0200

    Initial setup

---
+++ nbp/src/main/java/com/nbp/nbp/NbpApplication.java
@@ -0,0 +1,19 @@
+package com.nbp.nbp;
+
+import java.util.Collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(NbpApplication.class);
+        app.setDefaultProperties(Collections
+          .singletonMap("server.port", "8081"));
+        app.run(args);
+	}
+
+}
---
+++ nbp/src/test/java/com/nbp/nbp/NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbp.nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
